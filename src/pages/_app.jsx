2396f8c7f6d3ea731cbd6498a1c93cc6277d03d5
Arshaan <arshaan.bhimani@uwaterloo.ca>
2023-04-25 22:30:31 -0400

    GA tracking added to website

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,11 +1,25 @@
 import '@/styles/tailwind.css'
 import 'focus-visible'
+import Script from 'next/script'
 
 import { Analytics } from '@vercel/analytics/react'
 
 export default function App({ Component, pageProps }) {
+  const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_CONVERT_KIT_API_KEY
   return (
     <>
+      <Script
+        async
+        src={'https://www.googletagmanager.com/gtag/js?id=' + GA_MEASUREMENT_ID}
+      />
+      <Script>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}');
+        `}
+      </Script>
       <Component {...pageProps} />
       <Analytics />
     </>
